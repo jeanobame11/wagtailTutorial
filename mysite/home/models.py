f1f96a69fb0d3cae202e137b63753cf6101b896e
jeanobame11 <jeanobame99@gmail.com>
2025-11-14 00:20:31 -0600

    initial commit

---
+++ mysite/home/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+
+# import MultiFieldPanel:
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+
+    # import PublishingPanel:
+    PublishingPanel,
+)
+
+# import RichTextField:
+from wagtail.fields import RichTextField
+
+# import DraftStateMixin, PreviewableMixin, RevisionMixin, TranslatableMixin:
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+
+# import register_snippet:
+from wagtail.snippets.models import register_snippet
+
+
+class HomePage(Page):
+    # add the Hero section of HomePage:
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Homepage image",
+    )
+    hero_text = models.CharField(
+        blank=True,
+        max_length=255, help_text="Write an introduction for the site"
+    )
+    hero_cta = models.CharField(
+        blank=True,
+        verbose_name="Hero CTA",
+        max_length=255,
+        help_text="Text to display on Call to Action",
+    )
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Hero CTA link",
+        help_text="Choose a page to link to for the Call to Action",
+    )
+
+    body = RichTextField(blank=True)
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text"),
+                FieldPanel("hero_cta"),
+                FieldPanel("hero_cta_link"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel('body'),
+    ]
